b10b6ee9471d88b0bc205b1dbfd082fffd57b09b
Julia <maju.olivi@gmail.com>
2019-11-05 17:45:09 -0300

    files update

---
+++ geant.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+from datacenter.topology import GeantTopology
+from datacenter.profiles import IperfProfile
+from datacenter.profiles import PingProfile
+from datacenter.dc import Datacenter
+
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.node import RemoteController
+
+import signal
+import sys
+
+def signal_handler(signal, frame):
+    net.stop()
+    sys.exit(0)
+
+bw = 10
+
+geant_topo = GeantTopology(bw=bw)
+
+tenants = []
+for h in range(0, 31):
+    tenants.append(IperfProfile(num_nodes=2, duration=80))
+    tenants.append(PingProfile(num_nodes=2, duration=60))
+
+dc = Datacenter()
+
+net = Mininet(topo=geant_topo, link=TCLink, controller=None, autoSetMacs=True)
+
+dc.setup(net.hosts, tenants)
+
+print('Configure your controller correctly before starting the simulation...')
+print('type <ENTER>')
+
+input()
+
+net.addController('rmController', controller=RemoteController,
+                  ip='127.0.0.1', port=6633)
+
+net.start()
+CLI(net)
+
+# Before starting the simulation, run a ping all.
+""" while net.pingAll() > 0:
+    continue
+
+net.iperf()
+
+signal.signal(signal.SIGINT, signal_handler) """
---
+++ monitor.py
@@ -0,0 +1,58 @@
+import requests
+import time
+import datetime
+from influxdb import InfluxDBClient
+import math
+
+
+BASE_URL_API = 'http://localhost:8181/onos/v1'
+
+class APIMonitor(object):
+    def __init__(self):        
+        #Cria e conecta ao cliente Onos settando:
+        #     Host: ip address where InfluxDB is installed
+        #     Port: 8086
+        #     Username: onos
+        #     Password: onos.password (rocks/onos)
+        self.db_client = InfluxDBClient('localhost', 8086, 'onos', 'onos', 'onos') 
+        
+        #Cria o banco de dados
+        self.db_client.create_database('onos')
+        self.username = 'onos'
+        self.password = 'rocks'
+
+    def get_stats(self):
+        res = requests.get(
+            BASE_URL_API + '/statistics/delta/ports',
+            auth=(self.username, self.password)
+        )
+
+        data = res.json()
+
+        for stats in data['statistics']:
+            json_body = {
+                "measurement": "utilization",
+                "tags": {
+                    "device": stats["device"],
+                },
+                "time": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                "fields": {
+                    "value": 0
+                }
+            }
+
+            for stat in stats['ports']:
+                json_body["fields"]["value"] = json_body["fields"]["value"] + \
+                                               math.ceil(stat["bytesReceived"]/stat["durationSec"])
+                json_body["fields"]["value"] = json_body["fields"]["value"] + \
+                                               math.ceil(stat["bytesSent"]/stat["durationSec"])
+
+            json_body = [json_body, ]
+            self.db_client.write_points(json_body, time_precision='ms')
+
+
+if __name__ == '__main__':
+    monitor = APIMonitor()
+    while(True):
+        time.sleep(5)
+        monitor.get_stats()
