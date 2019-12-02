7bfbfa1d4cf175841c7fd450c7db7143dd7acf48
Maria Julia <maju.olivi@gmail.com>
2019-12-02 11:40:13 -0300

    data adjustment and boxplot

--- learning.py
+++ learning.py
@@ -1,8 +1,11 @@
 #influx -database 'database_name' -execute 'SELECT * FROM table_name' -format csv > test.csv
+
+
 # Load the Pandas libraries with alias 'pd' 
 import pandas as pd 
 # Read data from file 'filename.csv' 
 # (in the same directory that your python process is based)
 # Control delimiters, rows, column names with read_csv (see later) 
-data = pd.read_csv("influx_database.csv") 
+data =  pd.read_csv("/home/mariajulia/Documentos/Github_repo/SDN-Monitor/db/influx_database.csv", delimiter = ';')
+print(data)
 # Preview the first 5 lines of the loaded data 
