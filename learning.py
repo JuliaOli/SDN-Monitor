479fec749bcd032ac86c26f6d48a6d80b5675fa2
Maria Julia <maju.olivi@gmail.com>
2019-12-02 13:01:00 -0300

    learning file update

--- learning.py
+++ learning.py
@@ -3,9 +3,21 @@
 
 # Load the Pandas libraries with alias 'pd' 
 import pandas as pd 
+import numpy as np
+import seaborn
+import matplotlib.pyplot as plt
+
+
+data =  pd.read_csv("/home/mariajulia/Documentos/Github_repo/SDN-Monitor/db/influx_database.csv", delimiter = ';')
+#https://machinelearningmastery.com/load-explore-time-series-data-python/
+#https://pandas.pydata.org/pandas-docs/version/0.22/generated/pandas.Series.from_csv.html
+ts = pd.Series(data, index=pd.date_range(start="2014-02-01", periods=n, freq="H"))
+
+
+fig, ax = plt.subplots(figsize=(12,5))
+seaborn.boxplot(ts.index.dayofyear, ts, ax=ax)
 # Read data from file 'filename.csv' 
 # (in the same directory that your python process is based)
 # Control delimiters, rows, column names with read_csv (see later) 
-data =  pd.read_csv("/home/mariajulia/Documentos/Github_repo/SDN-Monitor/db/influx_database.csv", delimiter = ';')
-print(data)
+
 # Preview the first 5 lines of the loaded data 
