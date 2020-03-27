8a4fa00b39e08bfcfdc65d214feed3238ee787b6
JuliaOli <maju.olivi@gmail.com>
2020-03-26 23:26:33 -0300

    Prediction ok but values are too big for the plots

--- src/learning.py
+++ src/learning.py
@@ -1,8 +1,8 @@
 #influx -database 'database_name' -execute 'SELECT * FROM table_name' -format csv > test.csv
 
-#http://cmdlinetips.com/2018/03/how-to-make-boxplots-in-python-with-pandas-and-seaborn/
-#https://www.inf.ufsc.br/~marcelo.menezes.reis/Cap4.pdf
-#https://towardsdatascience.com/the-akaike-information-criterion-c20c8fd832f2
+# http://cmdlinetips.com/2018/03/how-to-make-boxplots-in-python-with-pandas-and-seaborn/
+# https://www.inf.ufsc.br/~marcelo.menezes.reis/Cap4.pdf
+# https://towardsdatascience.com/the-akaike-information-criterion-c20c8fd832f2
 # https://www.dnsstuff.com/bandwidth-monitor 
 #
 # Load the Pandas libraries with alias 'pd' 
@@ -29,9 +29,18 @@ class LearningTest:
         self.series = pd.read_csv(self.data, delim_whitespace=True)
         self.stewise_model = ''
   
+    def timestamp_to_datetime(self, data):
+        # https://stackoverflow.com/questions/40023746/plotting-of-pandas-dataframe-and-xaxis-as-timestamp-produces-empty-plot
+        # Converts timestamp into datetime format and sets time as the index
+        # This function is necessary to plot a correct relation between time and values
+        data['time'] = pd.to_datetime(data['time'], unit='s')
+        data.set_index('time', inplace=True)
+
+
     def autocorrelation_plots(self):
         #plot the valuesXtime
         plt.plot(self.series['value'])
+
         #autocorrelation plot of the time series
         # Autocorrelation is the correlation of a signal with a delayed 
         # copy — or a lag — of itself as a function of the delay.
@@ -56,7 +65,6 @@ class LearningTest:
         # Model_fit can be any model class, ex.: ARIMA.
         residuals = DataFrame(model_fit.resid) # Residuals from our model
         residuals.plot()
-        plt.show()
         # Generate Kernel Density Estimate plot using Gaussian kernels.
         residuals.plot(kind='kde')
         plt.show()
@@ -70,8 +78,8 @@ class LearningTest:
         plt.plot(predictions, color='red')
         plt.show()
 
-    def aic_score(self):
-        self.stepwise_model = auto_arima(self.series, start_p=1, start_q=1,
+    def aic_score(self, data):
+        self.stepwise_model = auto_arima(data, start_p=1, start_q=1,
                            max_p=3, max_q=3, m=12,
                            start_P=0, seasonal=False,
                            d=1, D=1, trace=True,
@@ -80,38 +88,23 @@ class LearningTest:
                            stepwise=True)
         print(self.stepwise_model.aic())
 
-    def timestamp_to_datetime(self):
-        # https://stackoverflow.com/questions/40023746/plotting-of-pandas-dataframe-and-xaxis-as-timestamp-produces-empty-plot
-        # Converts timestamp into datetime format and sets time as the index
-        # This function is necessary to plot a correct relation between time and values
-        self.series['time'] = pd.to_datetime(self.series['time'], unit='s')
-        self.series.set_index('time', inplace=True)
-
-
     # Define the model by calling ARIMA() and passing in the p, d, and q parameters.
     # The model is prepared on the training data by calling the fit() function.
     # Predictions can be made by calling the predict() function and specifying 
     # the index of the time or times to be predicted.
 
     def multiple_forecasting_arima(self, data, p, d, q):
-        model = ARIMA(data, order=(1,1,1))
-        model_fit = model.fit(disp=0)
-        # For the default l_bfgs_b solver, disp controls the frequency 
-        # of the output during the iterations. disp < 0 means no output in this case.
-        # print(model_fit.summary())
-        residuals_plots()
 
-        X = series.values
+        # Re-creating the ARIMA model after each new observation is received
+        # rolling forecast is required given the dependence on observations 
+        # in prior time steps for differencing and the AR model
+        X = data['value']
         size = int(len(X) * 0.66)
         train, test = X[0:size], X[size:len(X)]
         history = [x for x in train]
         predictions = list()
-        
-        # Re-creating the ARIMA model after each new observation is received
-        # rolling forecast is required given the dependence on observations 
-        # in prior time steps for differencing and the AR model
         for t in range(len(test)):
-            model = ARIMA(history, order=(p,d,q))
+            model = ARIMA(history, order=(5,1,0))
             model_fit = model.fit(disp=0)
             output = model_fit.forecast()
             yhat = output[0]
@@ -119,20 +112,16 @@ class LearningTest:
             obs = test[t]
             history.append(obs)
             print('predicted=%f, expected=%f' % (yhat, obs))
-
         error = mean_squared_error(test, predictions)
         print('Test MSE: %.3f' % error)
-        return error
+        self.predictions_plots(test, predictions)
 
 if __name__ == "__main__":
     learning = LearningTest("/home/julia/Documentos/Laccan/utilization.csv")
     learning.series = learning.series.drop(['device', 'port'], axis=1)
-    learning.timestamp_to_datetime()
-    training_size = learning.series.size()
-    print(training_size)
-    #learning.autocorrelation_plots()
-    #learning.decomposion_plots()
-    #learning.aic_score()
+    learning.timestamp_to_datetime(learning.series)
+    learning.aic_score(learning.series)
+    learning.multiple_forecasting_arima(learning.series, 1, 1, 1)
     
 #     while(True):
 #         time.sleep(20)
